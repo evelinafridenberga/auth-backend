5fda2e15ca48b373b0a89cb3c4f020a84a0386cb
Evelīna Frīdenberga - Tomaša <evelnafridenberga-tomasa@Evelinas-MacBook-Air.local>
2023-10-02 16:05:14 +0200

    changes

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.post("/register", (request, response) => {
 
       user.tokens.push({ token });
 
-      return user.save();
+      // return user.save();
     })
     .then((result) => {
       response.status(201).send({
